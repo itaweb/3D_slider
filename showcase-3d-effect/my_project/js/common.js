4cd708bec826c5292d039d216dcbdd9c95f6e842
Сергей Щикунов <admod32@gmail.com>
2022-05-30 19:25:48 +0300

    Creat slider

--- showcase-3d-effect/my_project/js/common.js
+++ showcase-3d-effect/my_project/js/common.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const ShowSlider = new Swiper(".showcase__carousel", {
+    loop: true,
+    slidesPerView: 3,
+    speed: 1800,
+    centeredSlides: true,
+  });
+
   document.querySelector("video").playbackRate = 2;
 });
